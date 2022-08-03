bdf776cb045f701be8008298749916aff98a4269
APakaeva <103844454+APakaeva@users.noreply.github.com>
2022-08-04 00:50:08 +0300

    Update CashbackHackServiceTest.java

--- ru/netology/service/CashbackHackServiceTest.java
+++ ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,47 @@
 package ru.netology.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-class CashbackHackServiceTest {
+import org.testng.annotations.Test;
 
+import static org.testng.Assert.*;
+
+public class CashbackHackerServiceTest {
+
+    @Test
+    public void shouldCashbackHackerServiceTest() {
+        CashbackHackerService service = new CashbackHackerService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldThousandCashbackHackerServiceTest() {
+        CashbackHackerService service = new CashbackHackerService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldZeroCashbackHackerServiceTest() {
+        CashbackHackerService service = new CashbackHackerService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldMinusCashbackHackerServiceTest() {
+        CashbackHackerService service = new CashbackHackerService();
+        int amount = -1;
+        int actual = service.remain(amount);
+        int expected = 1001;
+        assertEquals(actual, expected);
+
+    }
 }
